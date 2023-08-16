278a9304877e504c1cdc8daa1a01bb23d95e592d
Diyar Abbas <30482423+diyarit@users.noreply.github.com>
2023-08-16 10:40:01 -0500

    Update install_cpanel.sh
    too large join_buffer_size

--- install_cpanel.sh
+++ install_cpanel.sh
@@ -718,7 +718,6 @@ echo "Configuring MySQL..."
 sed -i '/^local-infile.*/d' /etc/my.cnf
 sed -i '/^query_cache_type.*/d' /etc/my.cnf
 sed -i '/^query_cache_size.*/d' /etc/my.cnf
-sed -i '/^join_buffer_size.*/d' /etc/my.cnf
 sed -i '/^tmp_table_size.*/d' /etc/my.cnf
 sed -i '/^max_heap_table_size.*/d' /etc/my.cnf
 sed -i '/^sql_mode.*/d' /etc/my.cnf
@@ -729,7 +728,6 @@ sed  -i '/\[mysqld\]/a sql_mode = ALLOW_INVALID_DATES,NO_ENGINE_SUBSTITUTION' /e
 sed  -i '/\[mysqld\]/a local-infile=0' /etc/my.cnf
 sed  -i '/\[mysqld\]/a query_cache_type=1' /etc/my.cnf
 sed  -i '/\[mysqld\]/a query_cache_size=12M' /etc/my.cnf
-sed  -i '/\[mysqld\]/a join_buffer_size=12M' /etc/my.cnf
 sed  -i '/\[mysqld\]/a tmp_table_size=192M' /etc/my.cnf
 sed  -i '/\[mysqld\]/a max_heap_table_size=256M' /etc/my.cnf
 sed  -i '/\[mysqld\]/a # WNPower pre-configured values' /etc/my.cnf
